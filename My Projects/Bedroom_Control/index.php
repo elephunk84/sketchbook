5d3dc5cb3e23a10176d988a8ef743063e21e8e23
elephunk84 <iaincstott@gmail.com>
2016-07-20 22:50:07 +0100

    update

--- My Projects/Bedroom_Control/index.php
+++ My Projects/Bedroom_Control/index.php
@@ -13,7 +13,7 @@ Main Lights -
 
     <?php
     if (isset($_POST['advance'])){
-	    exec('sudo python /var/www/html/1.py');
+	    exec('python /var/www/html/1.py');
 	}
     ?>
     <form action="" method="post" align="center">
@@ -24,7 +24,7 @@ Main Lights -
     
     <?php
     if (isset($_POST['iainslights'])){
-	    exec('sudo python /var/www/html/2.py');
+	    exec('python /var/www/html/2.py');
 	}
     ?>
     <form action="" method="post" align="center">
@@ -35,7 +35,7 @@ Main Lights -
     
     <?php
     if (isset($_POST['eloraslights'])){
-	    exec('sudo python /var/www/html/3,py');
+	    exec('python /var/www/html/3,py');
 	}
     ?>
     <form action="" method="post" align="center">
