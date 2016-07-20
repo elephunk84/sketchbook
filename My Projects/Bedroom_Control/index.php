dfcf18a5e25ce0e06b76dc87f6eea4fca39d7cd8
elephunk84 <iaincstott@gmail.com>
2016-07-20 23:44:18 +0100

    update

--- My Projects/Bedroom_Control/index.php
+++ My Projects/Bedroom_Control/index.php
@@ -12,33 +12,33 @@ Main Lights -
 </h2>
 
     <?php
-    if (isset($_POST['advance'])){
+    if (isset($_GET['advance'])){
 	    exec('/usr/bin/python /var/www/html/1.py');
 	}
     ?>
-    <form action="" method="post" align="center">
+    <form action="" method="get" align="center">
     <button type="submit" name="advance" id="submit">Advance</button>
     </form>
     <h2 align="center">
     Iains Lights - 
     
     <?php
-    if (isset($_POST['iainslights'])){
+    if (isset($_GET['iainslights'])){
 	    exec('/usr/bin/python /var/www/html/2.py');
 	}
     ?>
-    <form action="" method="post" align="center">
+    <form action="" method="get" align="center">
     <button type="submit" name="iainslights" id="iainslights">Iains Lights</button>
     </form>
     <h2 align="center">
     Eloras Lights - 
     
     <?php
-    if (isset($_POST['eloraslights'])){
+    if (isset($_GET['eloraslights'])){
 	    exec('/usr/bin/python /var/www/html/3.py');
 	}
     ?>
-    <form action="" method="post" align="center">
+    <form action="" method="get" align="center">
     <button type="submit" name="eloraslights" id="eloraslights">Eloras Lights</button>
     </form>
 
