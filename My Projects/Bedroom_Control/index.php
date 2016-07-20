46c50936177666990c00b8b61e9feec6fe52a407
elephunk84 <iaincstott@gmail.com>
2016-07-20 22:20:33 +0100

    update

--- My Projects/Bedroom_Control/index.php
+++ My Projects/Bedroom_Control/index.php
@@ -1,48 +1,6 @@
 <html>
-
-<head>
-<link rel="stylesheet" href="style.css">
-<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-<title>IBedroom Lighting Controll</title>
-</head>
-
-<body>
-<h2 align="center">
-Main Lights - 
-</h2>
-
-    <?php
-    if (isset($_POST['advance'])){
-	    exec('python /var/www/html/1.py');
-	}
-    ?>
-    <form action="" method="post" align="center">
-    <button type="submit" name="advance" id="submit">Advance</button>
-    </form>
-    <h2 align="center">
-    Iains Lights - 
-    
-    <?php
-    if (isset($_POST['iainslights'])){
-	    exec('python /var/www/html/2.py');
-	}
-    ?>
-    <form action="" method="post" align="center">
-    <button type="submit" name="iainslights" id="iainslights">Iains Lights</button>
-    </form>
-    <h2 align="center">
-    Eloras Lights - 
-    
-    <?php
-    if (isset($_POST['eloraslights'])){
-	    exec('python /var/www/html/3,py');
-	}
-    ?>
-    <form action="" method="post" align="center">
-    <button type="submit" name="eloraslights" id="eloraslights">Eloras Lights</button>
-    </form>
-
-</div>
-</body>
-
+<h1>Test Page 1</h1>
+<form name="input" action="/1.py" method="get">
+<input type="submit" value="Submit">
+</form>
 </html>
