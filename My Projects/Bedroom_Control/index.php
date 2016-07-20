e3e8e5a8e9e4a14fb4be5baf5cac08f1998a5aa2
elephunk84 <iaincstott@gmail.com>
2016-07-20 23:47:56 +0100

    update

--- My Projects/Bedroom_Control/index.php
+++ My Projects/Bedroom_Control/index.php
@@ -11,20 +11,20 @@
 Main Lights - 
 </h2>
 
-    <form action="/usr/bin/python /var/www/html/1.py" method="post" align="center">
+    <form action="/var/www/html/1.py" method="post" align="center">
     <button type="submit" name="advance" id="submit">Advance</button>
     </form>
     <h2 align="center">
     Iains Lights - 
     
     
-    <form action="/usr/bin/python /var/www/html/2.py" method="get" align="center">
+    <form action="/var/www/html/2.py" method="get" align="center">
     <button type="submit" name="iainslights" id="iainslights">Iains Lights</button>
     </form>
     <h2 align="center">
     Eloras Lights - 
     
-    <form action="/usr/bin/python /var/www/html/3.py" method="get" align="center">
+    <form action="/var/www/html/3.py" method="get" align="center">
     <button type="submit" name="eloraslights" id="eloraslights">Eloras Lights</button>
     </form>
 
