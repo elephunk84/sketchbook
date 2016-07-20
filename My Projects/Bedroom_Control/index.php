a14106813d38cfbd66060ab91df108cb42f7a4e0
elephunk84 <iaincstott@gmail.com>
2016-07-20 21:52:49 +0100

    update

--- My Projects/Bedroom_Control/index.php
+++ My Projects/Bedroom_Control/index.php
@@ -3,7 +3,7 @@
 <head>
 <link rel="stylesheet" href="style.css">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-<title>IBedroom Lighting Controll</title>
+<title>Bedroom Lighting Control</title>
 </head>
 
 <body>
@@ -11,34 +11,19 @@
 Main Lights - 
 </h2>
 
-    <?php
-    if (isset($_POST['advance'])){
-	    exec('1.py');
-	}
-    ?>
-    <form action="" method="post" align="center">
+    <form action="./1.py" method="post" align="center">
     <button type="submit" name="advance" id="submit">Advance</button>
     </form>
     <h2 align="center">
     Iains Lights - 
     
-    <?php
-    if (isset($_POST['iainslights'])){
-	    exec('2.py');
-	}
-    ?>
-    <form action="" method="post" align="center">
+    <form action="./2.py" method="post" align="center">
     <button type="submit" name="iainslights" id="iainslights">Iains Lights</button>
     </form>
     <h2 align="center">
     Eloras Lights - 
     
-    <?php
-    if (isset($_POST['eloraslights'])){
-	    exec('3,py');
-	}
-    ?>
-    <form action="" method="post" align="center">
+    <form action="./3.py" method="post" align="center">
     <button type="submit" name="eloraslights" id="eloraslights">Eloras Lights</button>
     </form>
 
