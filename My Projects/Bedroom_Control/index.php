2f970c5824d808d7e46d606c8ead8e5ebb34c0ce
elephunk84 <iaincstott@gmail.com>
2016-07-20 22:44:21 +0100

    update

--- My Projects/Bedroom_Control/index.php
+++ My Projects/Bedroom_Control/index.php
@@ -1,4 +1,48 @@
 <html>
-<h1>Test Page 1</h1>
-<input type="button" name="runmyscript" value=" Run Python code " onClick="<? exec('python 1.py'); ?>">
-</body></html>
+
+<head>
+<link rel="stylesheet" href="style.css">
+<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<title>IBedroom Lighting Controll</title>
+</head>
+
+<body>
+<h2 align="center">
+Main Lights - 
+</h2>
+
+    <?php
+    if (isset($_POST['advance'])){
+	    exec('python /var/www/html/1.py');
+	}
+    ?>
+    <form action="" method="post" align="center">
+    <button type="submit" name="advance" id="submit">Advance</button>
+    </form>
+    <h2 align="center">
+    Iains Lights - 
+    
+    <?php
+    if (isset($_POST['iainslights'])){
+	    exec('python /var/www/html/2.py');
+	}
+    ?>
+    <form action="" method="post" align="center">
+    <button type="submit" name="iainslights" id="iainslights">Iains Lights</button>
+    </form>
+    <h2 align="center">
+    Eloras Lights - 
+    
+    <?php
+    if (isset($_POST['eloraslights'])){
+	    exec('python /var/www/html/3,py');
+	}
+    ?>
+    <form action="" method="post" align="center">
+    <button type="submit" name="eloraslights" id="eloraslights">Eloras Lights</button>
+    </form>
+
+</div>
+</body>
+
+</html>
