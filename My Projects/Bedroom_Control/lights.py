f144b56484ba35fee5ce5fbb3a0c4f5d6afd1b2c
elephunk84 <iaincstott@gmail.com>
2016-07-20 21:42:13 +0100

    update

--- My Projects/Bedroom_Control/lights.py
+++ My Projects/Bedroom_Control/lights.py
@@ -7,21 +7,51 @@ laptops = form.getvalue('laptops','0')
 desktops = form.getvalue('desktops','0')
 print """
 <html>
+
+<head>
+<link rel="stylesheet" href="style.css">
+<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<title>IBedroom Lighting Controll</title>
+</head>
+
 <body>
-<form action='second.py'>
-How many laptops do you own?&nbsp;
-<input type='radio' checked name='laptops' value='0' />0&nbsp;
-<input type='radio' name='laptops' value='1' />1&nbsp;
-<input type='radio' name='laptops' value='2' />2
-<p>
-How many desktops do you own?&nbsp;
-<input type='radio' checked name='desktops' value='0' />0&nbsp;
-<input type='radio' name='desktops' value='1' />1&nbsp;
-<input type='radio' name='desktops' value='2' />2
-<p>
-<input type='submit' />
-<p>
-You own %d computers.
-</form>
+<h2 align="center">
+Main Lights - 
+</h2>
+
+    <?php
+    if (isset($_POST['advance'])){
+	    exec('1.py');
+	}
+    ?>
+    <form action="" method="post" align="center">
+    <button type="submit" name="advance" id="submit">Advance</button>
+    </form>
+    <h2 align="center">
+    Iains Lights - 
+    
+    <?php
+    if (isset($_POST['iainslights'])){
+	    exec('2.py');
+	}
+    ?>
+    <form action="" method="post" align="center">
+    <button type="submit" name="iainslights" id="iainslights">Iains Lights</button>
+    </form>
+    <h2 align="center">
+    Eloras Lights - 
+    
+    <?php
+    if (isset($_POST['eloraslights'])){
+	    exec('3,py');
+	}
+    ?>
+    <form action="" method="post" align="center">
+    <button type="submit" name="eloraslights" id="eloraslights">Eloras Lights</button>
+    </form>
+
+</div>
 </body>
-</html>""" % (int(laptops)+int(desktops))
+
+</html>
+"""
