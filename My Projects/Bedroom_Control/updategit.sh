ac7bc78475f7285ade7ac76a2d2b33528152f343
elephunk84 <iaincstott@gmail.com>
2016-07-20 21:59:38 +0100

    update

--- My Projects/Bedroom_Control/updategit.sh
+++ My Projects/Bedroom_Control/updategit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo "Update Git Repo"
 git add --all
 git commit -m "update"
 git push origin master
+echo "Update Remote"
 ssh pi@192.168.0.131 'cd /home/pi/GitRepo/sketchbook/My\ Projects/Bedroom_Control/; git pull'
