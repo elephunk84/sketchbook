0481c64159d6ea57ce3f6cb6733b105e78a38255
elephunk84 <iaincstott@gmail.com>
2016-07-20 21:37:45 +0100

    update

--- My Projects/Bedroom_Control/lights.py
+++ My Projects/Bedroom_Control/lights.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python
+import cgi
 print "Content-type: text/html"
 print
-print "<html>"
-print "<center>Hello, Linux.com!</center>"
-print "</html>"
+form = cgi.FieldStorage()
+laptops = form.getvalue('laptops','0')
+desktops = form.getvalue('desktops','0')
+print """
+<html>
+<body>
+<form action='second.py'>
+How many laptops do you own?&nbsp;
+<input type='radio' checked name='laptops' value='0' />0&nbsp;
+<input type='radio' name='laptops' value='1' />1&nbsp;
+<input type='radio' name='laptops' value='2' />2
+<p>
+How many desktops do you own?&nbsp;
+<input type='radio' checked name='desktops' value='0' />0&nbsp;
+<input type='radio' name='desktops' value='1' />1&nbsp;
+<input type='radio' name='desktops' value='2' />2
+<p>
+<input type='submit' />
+<p>
+You own %d computers.
+</form>
+</body>
+</html>""" % (int(laptops)+int(desktops))
