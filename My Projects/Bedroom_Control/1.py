f4f5462bb59086c8429d68baf1d7b33ae01bc15a
elephunk84 <iaincstott@gmail.com>
2016-07-20 19:24:46 +0000

    update

---
+++ My Projects/Bedroom_Control/1.py
@@ -0,0 +1,8 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+from time import sleep
+import serial
+ser = serial.Serial('/dev/ttyACM0', 9600) # Establish the connection on a specific port
+ser.write('1')
+
--- My Projects/Bedroom_Control/Bedroom_Control.py
+++ My Projects/Bedroom_Control/Bedroom_Control.py
@@ -1,27 +1,11 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
 
-import smbus
-import time
-bus = smbus.SMBus(1)
-
-address = 0x04
-
-def writeNumber(value):
-	bus.write_byte(address, value)
-	return -1
-
-def readNumber():
-	number = bus.read_byte(address)
-	return number
-
+from time import sleep
+import serial
+ser = serial.Serial('/dev/ttyACM0', 9600) # Establish the connection on a specific port
+counter = 32 # Below 32 everything in ASCII is gibberish
 while True:
-	var = input("Enter 1 – 9: ")
-	if not var:
-		continue
-	writeNumber(var)
-	print "RPI: Hi Arduino, I sent you ", var
-	time.sleep(1)
-	number = readNumber()
-	print "Arduino: Hey RPI, I received a digit ", number
-	print
+     counter +=1
+     print ser.readline() # Read the newest output from the Arduino
+     sleep(.1) # Delay for one tenth of a second
