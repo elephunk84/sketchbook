85f5c6ffebc501a715e4326530c5d51c0e1f77ff
elephunk84 <iaincstott@gmail.com>
2016-07-21 19:46:30 +0100

    update

--- My Projects/Bedroom_Control/lights.py
+++ My Projects/Bedroom_Control/lights.py
@@ -8,10 +8,16 @@ from bottle import route, run, template
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-@route('/')
+@app.route('/')
 def index():
-    return template(index)
+       """Home page"""
 
+    info = {'title': 'Welcome Home!',
+            'content': 'Hello World'
+            }
+
+    return template('index_html.tpl', info)
+    
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     run(host='192.168.0.131', port=port, debug=True)
