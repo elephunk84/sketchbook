7a50f1d5655bfff44e99443715c87d9757d8fcfd
elephunk84 <iaincstott@gmail.com>
2016-07-21 12:06:45 +0100

    update

--- My Projects/Bedroom_Control/lights.py
+++ My Projects/Bedroom_Control/lights.py
@@ -5,8 +5,9 @@ from time import sleep
 import serial
 ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
-    while 1:
-        print ser.readline()
+    print ser.readline()
+    print ser.readline()
+    print ser.readline()
     num = raw_input('Number:')
     if num == '1':
         ser.write('1')
