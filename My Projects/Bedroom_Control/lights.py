796872781ce71d1e3ed856a8bbe451318b4b114b
elephunk84 <iaincstott@gmail.com>
2016-07-21 20:58:28 +0100

    update

--- My Projects/Bedroom_Control/lights.py
+++ My Projects/Bedroom_Control/lights.py
@@ -11,8 +11,12 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
    return 'Hello World'
+   
+@app.route(‘/hello’)
+def hello_world():
+   return ‘hello world’
 
 if __name__ == '__main__':
    app.run('0.0.0.0')
