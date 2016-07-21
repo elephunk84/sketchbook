615ce609471fb93cdceab05c7307a0a04975c44d
elephunk84 <iaincstott@gmail.com>
2016-07-21 11:29:37 +0100

    update

--- My Projects/Bedroom_Control/lights.py
+++ My Projects/Bedroom_Control/lights.py
@@ -5,9 +5,10 @@ from time import sleep
 import serial
 ser = serial.Serial('/dev/ttyACM0', 9600)
 num = raw_input('Number:')
-if num == '1':
-    ser.write('1')
-if num == '2':
-    ser.write('2')
-if num == '3':
-    ser.write('3')
+while True:
+    if num == '1':
+        ser.write('1')
+    if num == '2':
+        ser.write('2')
+    if num == '3':
+        ser.write('3')
