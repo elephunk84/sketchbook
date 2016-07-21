45f61ccb595ed2d02b2828d260558fa640923385
elephunk84 <iaincstott@gmail.com>
2016-07-21 20:54:30 +0100

    update

--- My Projects/Bedroom_Control/lights.py
+++ My Projects/Bedroom_Control/lights.py
@@ -4,22 +4,15 @@
 from time import sleep
 import serial
 import os
-from bottle import route, run, template, static_file, url
+from flask import Flask
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-@route('/static/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='static/')
-        
-@route('/')
-def index():
-    """Home page"""
-    info = {'title': 'Welcome Home!',
-        'content': 'Hello World'
-    }
-    return template('index_html.tpl', info)
-    
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello World’
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    run(host='192.168.0.131', port=port, debug=True)
+   app.run()
